29111233a911930e965e9fd037ad20965ec807c6
lefasom <leosombra89@gmail.com>
2024-11-26 21:54:32 -0300

    ux

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,12 @@ export default function Home() {
       <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold text-slate-300">
         Suscríbete a tus plataformas favoritas
       </h2>
+      <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold text-slate-400">
+        Tu centro de entretenimiento con las mejores plataformas de streaming.
+      </h2>
+      <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold text-slate-300">
+        Suscríbete a tus plataformas favoritas
+      </h2>
       <Card />
       <Footer />
     </div>
