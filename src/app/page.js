d5fb2871029fb4ad46df24b633b92f2a8ee54a42
lefasom <leosombra89@gmail.com>
2024-11-26 21:53:13 -0300

    ux

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,12 @@ export default function Home() {
     <div className="grid place-items-center">
       <Header />
       <Cart />
-      <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold  text-zinc-200"> Tu centro de entretenimiento con las mejores plataformas de streaming.</h2>
+      <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold text-slate-400">
+        Tu centro de entretenimiento con las mejores plataformas de streaming.
+      </h2>
+      <h2 className=" text-2xl w-[80%] mt-12 text-center font-bold text-slate-300">
+        Suscríbete a tus plataformas favoritas
+      </h2>
       <Card />
       <Footer />
     </div>
