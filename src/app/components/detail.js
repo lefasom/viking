879ff362002c614a7938896e0183650ee64e8185
lefasom <leosombra89@gmail.com>
2024-11-30 22:02:38 -0300

    ux

--- src/app/components/detail.js
+++ src/app/components/detail.js
@@ -64,60 +64,60 @@ const Cart = () => {
 
         // El mensaje se formatea con salto de línea (%0A) y se genera el enlace
         const message = `${greeting}%0A%0ADetalles del pedido:%0A${itemsDetails}%0A%0ATotal: ${total} ARS%0A%0AGracias por tu atención!`;
-        
+
         return `https://wa.me/2616615057?text=${message}`;
     };
 
     return (
         <>
-        
-                <div className='fixed bg-black bg-opacity-80 z-50 flex right-0 top-0 w-full h-screen'>
-                    <div className='absolute right-3 top-3'>
-                        <Link
-                            className='text-cyan-50 p-3 text-2xl'
-                            href={'/'}
-                        >
-                            X
-                        </Link>
-                    </div>
-                    <div className="m-auto mt-[80px] w-[70%] p-4 max-w-md space-y-6 h-[50%] overflow-y-auto">
-                        <h2 className="absolute -z-10 top-0 left-0 right-0 p-6  text-2xl font-bold text-center text-gray-300">Tu carrito de compras</h2>
 
-                        {cartItems.length === 0 ? (
-                            <p className="text-center text-gray-500">Tu carrito está vacío.</p>
-                        ) : (
-                            <div className="space-y-2 ">
-                                {cartItems.map((item, index) => (
-                                    <div key={index} className="flex justify-between items-center bg-gray-800 text-white rounded-sm p-4">
-                                        <div>
-                                            <h3 className="text-lg font-semibold">{item.title}</h3>
-                                            <p className="text-sm">{item.price}</p>
-                                        </div>
-                                        <button
-                                            className="text-red-500 font-bold"
-                                            onClick={() => removeItem(index)}
-                                        >
-                                            Eliminar
-                                        </button>
-                                    </div>
-                                ))}
-
-                                <div className="absolute top-[67%] bg-gray-900 p-4 rounded-sm">
-                                    <h3 className="text-xl font-bold text-white mb-4">Total: {calculateTotal().toLocaleString()} ARS</h3>
-                                    <a
-                                        href={createWhatsAppMessage()} // Enlace para enviar el mensaje
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="m-4 w-full p-2 bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition duration-300"
-                                    >
-                                        Confirmar Pedido
-                                    </a>
-                                </div>
-                            </div>
-                        )}
-                    </div>
+            <div className='fixed bg-black bg-opacity-80 z-50 flex right-0 top-0 w-full h-screen'>
+                <div className='absolute right-3 top-3'>
+                    <Link
+                        className='text-cyan-50 p-3 text-2xl'
+                        href={'/'}
+                    >
+                        X
+                    </Link>
                 </div>
-           
+                <div className="m-auto mt-[80px] w-[70%] p-4 max-w-md space-y-6 h-[50%] overflow-y-auto">
+                    <h2 className="absolute -z-10 top-0 left-0 right-0 p-6  text-2xl font-bold text-center text-gray-300">Tu carrito de compras</h2>
+
+                    {cartItems.length === 0 ? (
+                        <p className="text-center text-gray-500">Tu carrito está vacío.</p>
+                    ) : (
+                        <div className="space-y-2 ">
+                            {cartItems.map((item, index) => (
+                                <div key={index} className="flex justify-between items-center bg-gray-800 text-white rounded-sm p-4">
+                                    <div>
+                                        <h3 className="text-lg font-semibold">{item.title}</h3>
+                                        <p className="text-sm">{item.price}</p>
+                                    </div>
+                                    <button
+                                        className="text-red-500 font-bold"
+                                        onClick={() => removeItem(index)}
+                                    >
+                                        Eliminar
+                                    </button>
+                                </div>
+                            ))}
+
+                            <div className="absolute top-[67%] left-[30%] bg-gray-900 p-4 rounded-sm">
+                                <h3 className="text-xl font-bold text-white mb-4">Total: {calculateTotal().toLocaleString()} ARS</h3>
+                                <a
+                                    href={createWhatsAppMessage()} // Enlace para enviar el mensaje
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="m-4 w-full p-2 bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition duration-300"
+                                >
+                                    Confirmar Pedido
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
         </>
     );
 };
