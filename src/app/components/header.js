560e3cc61e450f50b1175c86f1ac754b70475e68
lefasom <leosombra89@gmail.com>
2024-12-22 14:14:53 -0300

    tm mejora

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -27,7 +27,7 @@ export default function Header() {
         <nav className="flex justify-center space-x-6">
           <ul className="flex space-x-8">
             <li>
-              <a href="https://t.me/@josesitocu" target="_blank" rel="noopener noreferrer" className="flex items-center text-slate-200 hover:text-blue-400 transition transform hover:scale-110 duration-300">
+              <a href="https://t.me/@Josesitocu" target="_blank" rel="noopener noreferrer" className="flex items-center text-slate-200 hover:text-blue-400 transition transform hover:scale-110 duration-300">
                 <FontAwesomeIcon icon={faTelegram} className="h-14 w-14" />
               </a>
             </li>
